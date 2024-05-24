5ab2c36c8de6cd65bdf58213d349d5581cfc2e8d
Grzegorz Pyrkowski <grz.pyrkowski@gmail.com>
2024-05-24 11:45:23 +0200

    Remove cors and set server hosting frontend

--- server/index.js
+++ server/index.js
@@ -1,6 +1,4 @@
 import express from "express";
-import corsMiddleware from "./cors/index.js";
-import cors from "cors";
 import { connectDatabase } from "./connection.js";
 import {matchRouter} from "./routes/match.route.js";
 import {betRouter} from "./routes/bet.route.js";
@@ -8,11 +6,16 @@ import {pointsRouter} from "./routes/points.route.js";
 import {changeStateToPendingIfMatchStarted} from "./schedules/changeStateToPendingIfMatchStarted.js";
 import {addDailyPoints} from "./schedules/addDailyPoints.js";
 const app = express();
+import * as path from "node:path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const buildPath = path.join(__dirname, '../client/build');
 
 //middleware
 app.use(express.json());
-app.options('*', cors());
-app.use(corsMiddleware);
+app.use(express.static(buildPath));
 
 //allow pass data from forms
 app.use(express.urlencoded({extended: "false"}));
@@ -21,6 +24,10 @@ app.use(express.urlencoded({extended: "false"}));
 app.use("/api/matches", matchRouter);
 app.use("/api/bets", betRouter);
 app.use("/api/points", pointsRouter);
+
+app.get('/*', (req, res) => {
+    res.sendFile('index.html', {root: buildPath});
+})
 
 connectDatabase().then(() => {
     app.listen(4000, () => {
